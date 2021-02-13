0b35ecea603320bc5500f1dc34933eab6b91820c
Komil <komildeveloper111@gmail.com>
2021-02-13 22:42:42 +0500

    creating form-input.component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
@@ -29,22 +31,22 @@ class SignIn extends Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={email}
             required
-            onChange={this.handleChange}
+            label='email'
+            handleChange={this.handleChange}
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name='password'
             type='password'
             value={password}
             required
-            onChange={this.handleChange}
+            label='password'
+            handleChange={this.handleChange}
           />
-          <label>Password</label>
 
           <input type='submit' value='Submit Form' />
         </form>

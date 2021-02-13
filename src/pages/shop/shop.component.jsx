97e60342d8e31ca4f2c47d4234bdc4cb845680c7
Komil <komildeveloper111@gmail.com>
2021-02-13 15:20:33 +0500

    updating our app with a shop page, collection-preview.component and collection-item.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import SHOP_DATA from './shop.data.js';
 
-import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends Component {
   state = {
     collections: SHOP_DATA
   };
+
   render() {
     const { collections } = this.state;
-
     return (
       <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
